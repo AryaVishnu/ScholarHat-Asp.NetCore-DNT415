64e3a0752d380212b86898c6782e547a872392c7
AryaVishnu <33745752+AryaVishnu@users.noreply.github.com>
2023-03-10 16:27:58 +0000

    Add files via upload

---
+++ PostViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Module1.Models
+{
+    public class PostViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Body { get; set; }
+
+    }
+}
