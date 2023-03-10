632abd8c4ec509c893580767f004dd5860869b24
AryaVishnu <33745752+AryaVishnu@users.noreply.github.com>
2023-03-10 16:25:22 +0000

    Add files via upload

---
+++ PostController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Module1.Models;
+
+namespace Module1.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Post()
+        {
+            PostViewModel pvModel = new PostViewModel()
+            {
+                Id = 1,
+                Title = "What is ASP.NET Core",
+                Author = "Shailendra Chauhan",
+                Body = "ASP.NET Core is an open-source framework for building UI and API..."
+            };
+
+            return View(pvModel);
+        }
+    }
+}
